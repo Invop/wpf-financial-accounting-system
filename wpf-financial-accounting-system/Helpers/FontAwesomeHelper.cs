0d24eb128861078451adc6c17de87606a618a38d
Andrii Shandryk <shandrykjj512@gmail.com>
2023-11-04 15:22:03 +0200

    Menu btns

---
+++ wpf-financial-accounting-system/Helpers/FontAwesomeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace wpf_financial_accounting_system.Helpers;
+
+public static class FontAwesomeHelper
+{
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.RegisterAttached(
+            "Icon",
+            typeof(string),
+            typeof(FontAwesomeHelper),
+            new PropertyMetadata(string.Empty));
+
+    public static string GetIcon(UIElement element)
+    {
+        return (string)element.GetValue(IconProperty);
+    }
+
+    public static void SetIcon(UIElement element, string value)
+    {
+        element.SetValue(IconProperty, value);
+    }
+}
